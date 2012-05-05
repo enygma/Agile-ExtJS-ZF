8df596e225fd29cc1f69fb827547f17e86e80e00
Chris Cornutt <enygma@phpdeveloper.org>
2012-05-05 13:54:03 -0500

    adding SQL file to initialize the database for the example tables

---
+++ init.sql
@@ -0,0 +1,20 @@
+-- initialize our tables
+
+CREATE TABLE `users` (
+	name VARCHAR(100),
+	password VARCHAR(32),
+	ID INT NOT NULL AUTO_INCREMENT,
+	PRIMARY KEY(ID)
+);
+
+CREATE TABLE `chat_user` (
+	username VARCHAR(100),
+	ID INT NOT NULL AUTO_INCREMENT,
+	PRIMARY KEY(ID)
+);
+CREATE TABLE `chat_messages` (
+	message TEXT,
+	chatuser_id INT,
+	ID INT NOT NULL AUTO_INCREMENT,
+	PRIMARY KEY(ID)
+);
