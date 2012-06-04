4fdc8c29f601ce113adcbb1cbb832541c16e4fd8
Chris Cornutt <enygma@phpdeveloper.org>
2012-06-03 20:54:26 -0500

    updating example6 with comments

--- zfapp/public/js/examples/example6/app.js
+++ zfapp/public/js/examples/example6/app.js
@@ -1,12 +1,19 @@
+/**
+ * Create the application instance
+ */
 Ext.application({
+	/** Give the app a name */
 	name: 'example6',
 	
+	/** Set the default root path */
 	appFolder: '/js/examples/example6/app',
 
+	/** Autoload these controllers */
 	controllers: [
 		'Index'
 	],
 
+	/** Launch the application */
 	launch: function() {
 		console.log('Launch application "example6"');
 
@@ -14,6 +21,7 @@ Ext.application({
             layout: 'fit',
             items: [
                 {
+                	/** Reference to example6.view.index.Login view */
                 	xtype  : 'loginwin'
                 }
             ]
--- zfapp/public/js/examples/example6/app/controller/Index.js
+++ zfapp/public/js/examples/example6/app/controller/Index.js
@@ -1,7 +1,12 @@
+/**
+ * Default index controller
+ */
 Ext.define('example6.controller.Index', {
 	
+	/** Extend the default controller */
 	extend: 'Ext.app.Controller',
 
+	/** Autloaod views, models and stores */
 	views:  [
 		'index.Login'
 	],
@@ -10,6 +15,7 @@ Ext.define('example6.controller.Index', {
 	stores: [
 	],
 
+	/** Initialize the controller */
 	init: function() {
 		console.log('Index controller');
 	}
